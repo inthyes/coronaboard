a0628de90f7ecc9dc22322fea555c78cba931914
unknown <hyesoo0806@gmail.com>
2023-07-05 18:00:00 +0900

    feat: api 구현 및 구글시트 api 연동(#01)

---
+++ coronaboard-api/database/index.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+
+//데이터베이스 연결 정보 설정
+const config = {
+    host : '127.0.0.1',
+    port : 3306,
+    database : 'coronaboard',
+    user : 'coronaboard_admin',
+    password : '1234',
+};
+
+// 데이터베이스 연결 정보를 입력해 시퀄라이즈 인스턴스 생성
+const sequelize = new Sequelize(config.database, config.user, config.password,{
+    host : config.host,
+    dialect : 'mysql',
+});
+
+// 외부 모듈에서 사용할 수 있도록 내보내기
+module.exports = {
+    sequelize,
+    // 데이터베이스 연결이 완료된 객체 모델 완성
+    GlobalStat: require('./global-stat.model')(sequelize),
+    KeyValue: require('./key-value.model')(sequelize),
+};
+
+
---
+++ coronaboard-api/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {sequelize} = require('./database');
+const globalStatController = require('./controller/global-stat.controller');
+const KeyValueControlloer = require('./controller/key-value.controller');
+const keyValueController = require('./controller/key-value.controller');
+// const wrapWithErrorHandler = require('./util');
+
+async function launchServer(){
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.get('/',(req, res)=> {
+        res.json({message: 'hello'});
+    });
+    try{
+        await sequelize.sync();
+        console.log('Database is ready!');
+    } catch (error){
+        console.log('Unable to connect to the database');
+        console.log(error);
+        process.exit(1);
+    }
+
+    app.get('/global-stats', globalStatController.getAll);
+    app.post('/global-stats',globalStatController.insertOrUpdate);
+    app.delete('/global-stats',globalStatController.remove);
+
+    app.get('/key-value',keyValueController.get);
+    app.post('/key-value',keyValueController.insertOrUpdate);
+    app.delete('/key-value',keyValueController.remove);
+
+    const port = 8080;
+    app.listen(port, () => {
+        console.log(`server run ${port}`);
+    })
+
+    
+}
+
+launchServer();
